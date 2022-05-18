0dd9b414bd27e94fbc12992aaa5f1cfe855f8991
Qibin <qbye0405@gmail.com>
2022-05-18 11:42:15 -0400

    commit tree 01619c59040eb87d9641883c28b6136d23f3c742

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+customerName;
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'ok bob';
+
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'alright bob';
+}
+
+
